1b12eb0c9cd787d7bd3c90a8c8aec6b0f1f57b49
Sam Vasserman <samvasserman@Sams-MacBook-Pro-2.local>
2024-03-14 19:19:13 -0400

    cleanup of real dataset

--- data/real_scraping/scraper.py
+++ data/real_scraping/scraper.py
@@ -1,14 +1,23 @@
+#!/usr/bin/env python
+
 import requests
+from bs4 import BeautifulSoup as bs
+import os
+import pandas as pd
+import re
+
+current_dir = './data/real_scraping'
+dataset_dir = './data/datasets'
 
 def get_image_from_article(row):
-    title = row['Headline']
+    title = row['Title']
     response = requests.get(row['Url'])
     soup = bs(response.content, features='html.parser')
-        
-    picture_element = soup.find('picture', class_='image__picture')
 
-    if picture_element:
-        img_tag = picture_element.find('img')
+    container_element = soup.find('div', class_='image__container')
+
+    if container_element:
+        img_tag = container_element.find('img')
 
         img_url = img_tag['src'] if img_tag and 'src' in img_tag.attrs else None
 
@@ -18,15 +27,42 @@ def get_image_from_article(row):
     if img_url:
         img_response = requests.get(img_url)
 
-        os.makedirs('cnn-images', exist_ok=True)
+        os.makedirs(f'{current_dir}/images', exist_ok=True)
 
-        img_filename = f"cnn-images/{title.replace(' ', '_').replace('/', '_')}.jpg"
-        
+        title = title.replace(" ", "_").replace(".", "_")
+        img_filename = f"{current_dir}/images/{title}.jpg"
+
         with open(img_filename, 'wb') as img_file:
             img_file.write(img_response.content)
-        
+
         img_filename = img_filename.split('/')[1]
     else:
         img_filename = 'no_image_available.jpg'  # Example placeholder
 
     return img_filename
+
+def sanitize_text(text):
+  pattern = r'\(\s*CNN\s*.*?\)|\[\s*CNN\s*.*?\]'
+  text = re.sub(pattern, '', text)
+  return text
+
+def sanitize_title(title):
+  return title[:-6]
+
+if __name__ == "__main__":
+
+    # get the df
+    df = pd.read_csv(f'{current_dir}/CNN_Articels_clean.csv')
+    df = df.iloc[:250]
+
+    # reformatting
+    df.rename(columns={'Headline': 'Title', 'Article text': 'Text'}, inplace=True)
+
+    df.loc[:, 'Text'] = df.apply(lambda row: sanitize_text(row['Text']), axis=1) # sanitize text
+    df.loc[:, 'Title'] = df.apply(lambda row: sanitize_title(row['Title']), axis=1) # sanitize titles
+    df['Image'] = df.apply(lambda row : get_image_from_article(row), axis=1) # get images
+
+    df['Label'] = 0 # add labels
+    df = df[['Title','Text','Label', 'Image']] # strip uneccessary columns
+
+    df.to_csv(f"{current_dir}/real_dataset.csv")
